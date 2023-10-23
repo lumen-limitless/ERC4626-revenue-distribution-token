89901ab6f17ca7913df2da226d51293efcaff2f0
lumen-limitless <lumenlimitless@gmail.com>
2023-10-23 16:09:40 -0400

    add deployment script

--- script/RevenueDistributionToken.s.sol
+++ script/RevenueDistributionToken.s.sol
@@ -1,9 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.20;
+
+import {RevenueDistributionToken} from "src/RevenueDistributionToken.sol";
 
 import "forge-std/Script.sol";
 
-contract Name is Script {
-    function run() public {
+contract RevenueDistributionTokenScript is Script {
+    function run() external returns (RevenueDistributionToken deployment) {
         vm.startBroadcast();
+
+        deployment =
+        new RevenueDistributionToken("test", "test", address(1), address(0x476908D9f75687684CE3DBF6990e722129cDbCc6), 1e30);
+
+        vm.stopBroadcast();
     }
 }
